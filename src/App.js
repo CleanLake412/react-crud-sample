74e14a44e52952c62b4510176cf350ffcc381355
CleanLake <three_star123@hotmail.com>
2020-06-28 00:13:51 +0900

    adopt withStyles module

--- src/App.js
+++ src/App.js
@@ -2,11 +2,24 @@ import React, { Component } from 'react';
 //import logo from './logo.svg';
 import Customer from './components/Customer'
 import './App.css';
+import Paper from '@material-ui/core/Paper';
 import Table from '@material-ui/core/Table';
 import TableHead from '@material-ui/core/TableHead';
 import TableBody from '@material-ui/core/TableBody';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = theme => ({
+    root: {
+        width: "100%",
+        marginTop: theme.spacing.unit * 3,
+        overflowX: "auto"
+    },
+    table: {
+        minWidth: 1080
+    }
+});
 
 const customers = [
     {
@@ -37,9 +50,10 @@ const customers = [
 
 class App extends Component {
     render() {
+        const { classes } = this.props;
         return (
-            <div>
-                <Table>
+            <Paper className={classes.root}>
+                <Table className={classes.table}>
                     <TableHead>
                         <TableRow>
                             <TableCell>ID</TableCell>
@@ -56,9 +70,9 @@ class App extends Component {
                         })}
                     </TableBody>
                 </Table>
-            </div>
+            </Paper>
         );
     }
 }
 
-export default App;
+export default withStyles(styles)(App);
