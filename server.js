68b553bd5c4be963cf9b3e93eb8fc568c152b7ee
CleanLake <three_star123@hotmail.com>
2020-06-28 12:30:54 +0900

    fetch list of customers from mysql : npm install -S mysql

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -5,35 +6,29 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// MySQL connection
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: conf.host,
+    user: conf.user,
+    password: conf.password,
+    port: conf.port,
+    database: conf.database
+});
+connection.connect();
+
 app.get('/api/customers', (req, res) => {
-    setTimeout(function() {
-        res.send([
-            {
-                'id': 1,
-                'image': 'https://placeimg.com/64/64/1',
-                'name': '山本太郎',
-                'birthday': '961222',
-                'gender': '男性',
-                'job': '大学生'
-            },
-            {
-                'id': 2,
-                'image': 'https://placeimg.com/64/64/2',
-                'name': '花島三郎',
-                'birthday': '960508',
-                'gender': '男性',
-                'job': '開発者'
-            },
-            {
-                'id': 3,
-                'image': 'https://placeimg.com/64/64/3',
-                'name': '久保田',
-                'birthday': '961127',
-                'gender': '男性',
-                'job': 'デザイナー'
+    setTimeout(function () {
+        connection.query(
+            'SELECT * FROM CUSTOMER',
+            (err, rows, fields) => {
+                res.send(rows);
             }
-        ]);
-    }, 1000);
+        )
+    }, 500);
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
