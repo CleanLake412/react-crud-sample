d51cf969dc02fc04699e7d4c0662bef79ff4701d
CleanLake <three_star123@hotmail.com>
2020-06-28 13:01:12 +0900

    language

--- frontend/src/components/CustomerAdd.js
+++ frontend/src/components/CustomerAdd.js
@@ -70,12 +70,12 @@ class CustomerAdd extends React.Component {
     render() {
         return (
             <form onSubmit={this.handleFormSubmit}>
-                <h1>고객 추가</h1>
-                프로필 이미지: <input type="file" name="file" file={this.state.file} value={this.state.fileName} onChange={this.handleFileChange} /><br/>
-                이름: <input type="text" name="userName" value={this.state.userName} onChange={this.handleValueChange} /><br/>
-                생년월일: <input type="text" name="birthday" value={this.state.birthday} onChange={this.handleValueChange} /><br/>
-                성별: <input type="text" name="gender" value={this.state.gender} onChange={this.handleValueChange} /><br/>
-                직업: <input type="text" name="job" value={this.state.job} onChange={this.handleValueChange} /><br/>
+                <h1>New customer</h1>
+                Photo: <input type="file" name="file" file={this.state.file} value={this.state.fileName} onChange={this.handleFileChange} /><br/>
+                Name: <input type="text" name="userName" value={this.state.userName} onChange={this.handleValueChange} /><br/>
+                Birthday: <input type="text" name="birthday" value={this.state.birthday} onChange={this.handleValueChange} /><br/>
+                Gender: <input type="text" name="gender" value={this.state.gender} onChange={this.handleValueChange} /><br/>
+                Job: <input type="text" name="job" value={this.state.job} onChange={this.handleValueChange} /><br/>
                 <button type="submit">추가하기</button>
             </form>
         )
